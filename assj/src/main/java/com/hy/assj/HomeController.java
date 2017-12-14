39d74147a92d9dd0417fb46abcc6eb2b12951439
holypurejk <holypurejk@naver.com>
2017-12-14 15:22:05 +0900

    테스트 수정

--- assj/src/main/java/com/hy/assj/HomeController.java
+++ assj/src/main/java/com/hy/assj/HomeController.java
@@ -33,6 +33,8 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		System.out.println("수정연습");
+		System.out.println("나도 수정연습");
+		
 		
 		return "home";
 	}
